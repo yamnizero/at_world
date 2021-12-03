d4a98f60bfa2f4ed2f6620280b379b75daa91d80
Mohammed <yamni.zero@gmail.com>
2021-12-03 12:21:23 +0200

    d

--- lib/Models/Home_models/detail_member_models/timeline.dart
+++ lib/Models/Home_models/detail_member_models/timeline.dart
@@ -1,3 +1,4 @@
+import 'package:at_world/Models/Home_models/detail_member_models/widget_timeline.dart';
 import 'package:at_world/share/styles/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,9 +16,27 @@ class TimeLinePage extends StatelessWidget {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(
-            AntDesign.arrowleft,
-            color: black,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Icon(
+                  AntDesign.arrowleft,
+                  color: black,
+                ),
+                // SizedBox(width: 5,),
+                // Padding(
+                //   padding: const EdgeInsets.only(left: 35.0),
+                //   child: Text(
+                //     'Mohammed',
+                //     style: TextStyle(
+                //       color: black,
+                //       fontSize: 12
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
         title: Text(
@@ -29,9 +48,9 @@ class TimeLinePage extends StatelessWidget {
         centerTitle: true,
         actions: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.only(right: 10.0),
             child: CircleAvatar(
-                backgroundColor: kClub,
+                backgroundColor: Colors.grey.withOpacity(0.6),
                 radius: 15,
                 child: Icon(
                   Icons.border_color_rounded,
@@ -42,189 +61,7 @@ class TimeLinePage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 23.0,
-                          backgroundImage: AssetImage('assets/images/me3.png'),
-                        ),
-                        SizedBox(width: 15,),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Adele joined the group Teaching Ideas',
-                                  maxLines: 3,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: postText),
-                              SizedBox(height: 5,),
-                              Text('a year ago',style: TextStyle(fontSize: 14,color: Colors.grey),),
-
-                            ],
-                          ),
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(AntDesign.like1,size: 17.0,color: Colors.grey,),
-                            SizedBox(width: 5,),
-                            Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
-                            SizedBox(width: 15,),
-                            Icon(AntDesign.wechat,size: 17.0,color: Colors.grey,),
-                            SizedBox(width: 5,),
-                            Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(MaterialIcons.more_horiz,
-                              size: 15.0,color: Colors.grey,
-                            ),
-
-                          ],
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(height: 15,),
-                    Divider(height: 10,thickness: 1,)
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                 // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 23.0,
-                          backgroundImage: AssetImage('assets/images/me3.png'),
-                        ),
-                        SizedBox(width: 15,),
-
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Adele commented on the post, After Avengers:Endgame,Should Marvel take 2020 off?',
-                                  maxLines: 3,
-                                  overflow: TextOverflow.clip,
-                                  style: postText
-                              ),
-                              SizedBox(height: 5,),
-                              Text('a year ago',style: TextStyle(fontSize: 14,color: Colors.grey),),
-
-                            ],
-                          ),
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(height: 20,),
-                    Column(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 80,
-                          decoration: BoxDecoration(
-                             color: Colors.grey.shade300,
-
-                            borderRadius: BorderRadius.only(
-                                topLeft:Radius.circular(20),
-
-                              topRight:  Radius.circular(20),
-                            ),
-                          ),
-                          child:  Padding(
-                            padding: const EdgeInsets.only(left: 30.0,right: 15.0),
-                            child: Center(
-                              child: Text('Adele commented on the post, After Avengers:Endgame,Should Marvel take 2020 off?',
-                                  maxLines: 3,
-                                  overflow: TextOverflow.clip,
-                                  style: postText
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.withOpacity(0.1),
-                            borderRadius: BorderRadius.only(
-                              bottomLeft:Radius.circular(20),
-                              bottomRight:  Radius.circular(20),
-                            ),
-                          ),
-                          child:  Padding(
-                            padding: const EdgeInsets.only(left: 30.0,right: 15.0),
-                            child: Row(
-                              children: [
-                                Icon(Ionicons.arrow_forward_circle_outline,color: Colors.grey,),
-                               SizedBox(width: 5,),
-                                Center(
-                                  child: Text('View Post',
-                                      maxLines: 3,
-                                      overflow: TextOverflow.clip,
-                                      style: postText
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(AntDesign.like1,size: 17.0,color: Colors.grey,),
-                            SizedBox(width: 5,),
-                            Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                             Icon(MaterialIcons.more_horiz,
-                               size: 15.0,color: Colors.grey,
-                             ),
-
-                          ],
-                        ),
-
-                      ],
-                    ),
-                    SizedBox(height: 15,),
-                    Divider(height: 10,thickness: 1,)
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: WidgetTimeLine(),
       ),
     );
   }
---
+++ lib/Models/Home_models/detail_member_models/widget_timeline.dart
@@ -0,0 +1,271 @@
+
+
+import 'package:at_world/share/styles/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+class WidgetTimeLine extends StatelessWidget {
+  const WidgetTimeLine({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 23.0,
+                      backgroundImage: AssetImage('assets/images/me3.png'),
+                    ),
+                    SizedBox(width: 15,),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Adele joined the group Teaching Ideas',
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                              style: postText),
+                          SizedBox(height: 5,),
+                          Text('a year ago',style: TextStyle(fontSize: 14,color: Colors.grey),),
+
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(AntDesign.like1,size: 17.0,color: Colors.grey,),
+                        SizedBox(width: 5,),
+                        Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
+                        SizedBox(width: 15,),
+                        Icon(AntDesign.wechat,size: 17.0,color: Colors.grey,),
+                        SizedBox(width: 5,),
+                        Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(MaterialIcons.more_horiz,
+                          size: 15.0,color: Colors.grey,
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Divider(height: 10,thickness: 1,)
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 23.0,
+                      backgroundImage: AssetImage('assets/images/me3.png'),
+                    ),
+                    SizedBox(width: 15,),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Adele joined the group Teaching Ideas',
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                              style: postText),
+                          SizedBox(height: 5,),
+                          Text('a year ago',style: TextStyle(fontSize: 14,color: Colors.grey),),
+
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Container(
+                 // color: Colors.red,
+                  width: MediaQuery.of(context).size.width,
+                  height:250,
+                child:   Image.asset(
+                  'assets/images/b.png',
+                  width: MediaQuery.of(context).size.width,
+                ),
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Row(
+                      children: [
+                        Icon(AntDesign.like1,size: 17.0,color: Colors.grey,),
+                        SizedBox(width: 5,),
+                        Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
+                        SizedBox(width: 15,),
+                        Icon(AntDesign.wechat,size: 17.0,color: Colors.grey,),
+                        SizedBox(width: 5,),
+                        Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(MaterialIcons.more_horiz,
+                          size: 15.0,color: Colors.grey,
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Divider(height: 10,thickness: 1,)
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 23.0,
+                      backgroundImage: AssetImage('assets/images/me3.png'),
+                    ),
+                    SizedBox(width: 15,),
+
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Adele commented on the post, After Avengers:Endgame,Should Marvel take 2020 off?',
+                              maxLines: 3,
+                              overflow: TextOverflow.clip,
+                              style: postText
+                          ),
+                          SizedBox(height: 5,),
+                          Text('a year ago',style: TextStyle(fontSize: 14,color: Colors.grey),),
+
+                        ],
+                      ),
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+
+                        borderRadius: BorderRadius.only(
+                          topLeft:Radius.circular(20),
+
+                          topRight:  Radius.circular(20),
+                        ),
+                      ),
+                      child:  Padding(
+                        padding: const EdgeInsets.only(left: 30.0,right: 15.0),
+                        child: Center(
+                          child: Text('Adele commented on the post, After Avengers:Endgame,Should Marvel take 2020 off?',
+                              maxLines: 3,
+                              overflow: TextOverflow.clip,
+                              style: postText
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.1),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft:Radius.circular(20),
+                          bottomRight:  Radius.circular(20),
+                        ),
+                      ),
+                      child:  Padding(
+                        padding: const EdgeInsets.only(left: 30.0,right: 15.0),
+                        child: Row(
+                          children: [
+                            Icon(Ionicons.arrow_forward_circle_outline,color: Colors.grey,),
+                            SizedBox(width: 5,),
+                            Center(
+                              child: Text('View Post',
+                                  maxLines: 3,
+                                  overflow: TextOverflow.clip,
+                                  style: postText
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(AntDesign.like1,size: 17.0,color: Colors.grey,),
+                        SizedBox(width: 5,),
+                        Text('0',style: TextStyle(color: Colors.grey,fontSize: 16,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(MaterialIcons.more_horiz,
+                          size: 15.0,color: Colors.grey,
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Divider(height: 10,thickness: 1,)
+              ],
+            ),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
